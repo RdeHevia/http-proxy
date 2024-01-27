43c3525a6d9934a12d0cadf1211b8d2f44c30905
Raul de hevia <55640085+RdeHevia@users.noreply.github.com>
2024-01-26 17:39:12 -0800

    wip

--- main.go
+++ main.go
@@ -22,19 +22,23 @@ ENPOINT_PORT=1234
 */
 
 func proxyRequest(connection net.Conn) {
-	requestData := make([]byte, 4000)
+	requestData := make([]byte, 40000) // RdH change
 	bytesRequest, err := connection.Read(requestData)
+	if bytesRequest == 0 {
+		log.Println("RdH request bytes 0")
+		return
+	}
 	if err != nil {
 		log.Println(err)
 		return // TBD
 	}
 
-	log.Printf(" ? -> %v ; %vB", connection.RemoteAddr(), bytesRequest)
+	log.Printf(" %v -> %v ; %vB", connection.LocalAddr(), connection.RemoteAddr(), bytesRequest)
 
 	connectionEndpoint, err := net.Dial("tcp", "localhost:9000")
 	connectionEndpoint.Write(requestData)
 	log.Printf(" %v -> %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesRequest)
-	responseData := make([]byte, 4000)
+	responseData := make([]byte, 40000)
 	bytesResponse, err := connectionEndpoint.Read(responseData)
 	if err != nil {
 		log.Println(err)
@@ -44,8 +48,34 @@ func proxyRequest(connection net.Conn) {
 	log.Printf(" %v <- %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesResponse)
 
 	connection.Write(responseData)
+	log.Printf(" %v <- %v ; %vB", connection.LocalAddr(), connection.RemoteAddr(), bytesResponse)
 
+	packageCount := 1
+	for {
+		log.Println("rdh iteration", packageCount)
+		bytesResponse, err = connectionEndpoint.Read(responseData)
+		log.Printf(" %v <- %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesResponse)
+
+		// if err != nil {
+		// 	log.Println(err)
+		// 	log.Println("rdh err!", err)
+		// 	return // TBD (Close too?)
+		// }
+		connection.Write(responseData)
+		log.Printf(" %v <- %v ; %vB", connection.LocalAddr(), connection.RemoteAddr(), bytesResponse)
+
+		if bytesResponse == 0 {
+			log.Println("rdh bytes 0")
+			break
+		}
+
+		packageCount++
+	}
+	log.Println("rdh here")
+	connectionEndpoint.Close()
+	log.Println("TCP connection with localhost:9000 closed")
 	connection.Close()
+	log.Printf("TCP connection with %v closed", connection.LocalAddr())
 }
 
 func main() {
@@ -58,6 +88,8 @@ func main() {
 
 	for {
 		connection, err := tcpListener.Accept()
+
+		log.Println("TCP Connection accepted", connection.LocalAddr())
 		if err != nil {
 			log.Println(err)
 			continue
