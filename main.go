bbd7566a7742bec115b203c926b5d6d07b8a8e92
Raul de hevia <55640085+RdeHevia@users.noreply.github.com>
2024-01-29 15:47:09 -0800

    fixed

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
 )
 
 func GenerateRandomString(length int) (string, error) {
@@ -21,6 +20,12 @@ func GenerateRandomString(length int) (string, error) {
 func print(id string, messageRaw string, v ...any) {
 	message := fmt.Sprintf(messageRaw, v...)
 	log.Printf("%v - %v", id, message)
+}
+
+func printMessage(id string, buffer []byte) {
+	// message := strings.ReplaceAll(string(buffer), "\r", "")
+	message := string(buffer)
+	print(id, message)
 }
 
 /*
@@ -39,111 +44,50 @@ ENDPOINT_HOST=localhost
 ENPOINT_PORT=1234
 */
 
-// func proxyRequest(connection net.Conn) {
-// 	connectionEndpoint, _ := net.Dial("tcp", "localhost:9000")
-
-// 	count := 0
-// 	requestData := make([]byte, 1500) // RdH change
-// 	for {
-// 		log.Println("rdh request forward count", count)
-// 		log.Println("rdh here1")
-// 		bytesRequest, _ := connection.Read(requestData)
-// 		log.Println("rdh here2")
-
-// 		// if err != nil {
-// 		// 	log.Println(err)
-// 		// 	break // TBD
-// 		// }
-// 		log.Printf(" %v -> %v ; %vB", connection.LocalAddr(), connection.RemoteAddr(), bytesRequest)
-
-// 		connectionEndpoint.Write(requestData)
-// 		log.Printf(" %v -> %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesRequest)
-
-// 		if bytesRequest == 0 {
-// 			log.Println("RdH request bytes 0")
-// 			break
-// 		}
-// 		count++
-// 	}
-
-// 	for {
-// 		responseData := make([]byte, 1500)
-// 		bytesResponse, _ := connectionEndpoint.Read(responseData)
-// 		log.Printf(" %v <- %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesResponse)
-// 		connection.Write(responseData)
-
-// 		log.Printf(" %v <- %v ; %vB", connection.LocalAddr(), connection.RemoteAddr(), bytesResponse)
-
-// 		if bytesResponse == 0 {
-// 			log.Println("rdh bytes 0")
-// 			break
-// 		}
-// 	}
-
-// 	connectionEndpoint.Close()
-// }
-
 func proxyRequest(id string, connection net.Conn) {
+	packageCount := 0
+	connectionEndpoint, _ := net.Dial("tcp", "localhost:9000")
+
 	requestData := make([]byte, 1500) // RdH change
-	bytesRequest, err := connection.Read(requestData)
+	bytesRequest, _ := connection.Read(requestData)
 	if bytesRequest == 0 {
 		// log.Println("RdH request bytes 0")
 		print(id, "RdH request bytes 0")
 		return
 	}
-	if err != nil {
-		log.Println(err)
-		return // TBD
-	}
 
-	print(id, " %v -> %v ; %vB", connection.LocalAddr(), connection.RemoteAddr(), bytesRequest)
+	print(id, " %v -> %v ; %vB", "?", connection.RemoteAddr(), bytesRequest)
+	printMessage(id, requestData)
 
-	connectionEndpoint, err := net.Dial("tcp", "localhost:9000")
 	connectionEndpoint.Write(requestData)
-
 	print(id, " %v -> %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesRequest)
-
-	responseData := make([]byte, 1500)
-	bytesResponse, err := connectionEndpoint.Read(responseData)
-	if err != nil {
-		log.Println(err)
-		return // TBD (Close too?)
-	}
-
-	print(id, " %v <- %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesResponse)
-
-	connection.Write(responseData)
-
-	print(id, " %v <- %v ; %vB", connection.LocalAddr(), connection.RemoteAddr(), bytesResponse)
-
-	packageCount := 1
+	// time.Sleep(100 * time.Millisecond) // Rdh this fixes some kind of racing condition. Investigate
 	for {
 		print(id, "rdh iteration - %v", packageCount)
-		bytesResponse, err = connectionEndpoint.Read(responseData)
-		print(id, " %v <- %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesResponse)
 
-		// if err != nil {
-		// 	log.Println(err)
-		// 	log.Println("rdh err!", err)
-		// 	return // TBD (Close too?)
-		// }
-		connection.Write(responseData)
-		print(id, " %v <- %v ; %vB", connection.LocalAddr(), connection.RemoteAddr(), bytesResponse)
+		responseData := make([]byte, 1500)
+		bytesResponse, _ := connectionEndpoint.Read(responseData)
+
+		print(id, " %v <- %v ; %vB", connection.RemoteAddr(), connectionEndpoint.RemoteAddr(), bytesResponse)
+		printMessage(id, responseData)
 
 		if bytesResponse == 0 {
 			print(id, "rdh bytes 0")
 			break
 		}
 
+		connection.Write(responseData[:bytesResponse])
+
+		print(id, " %v <- %v ; %vB", "?", connection.RemoteAddr(), bytesResponse)
+
 		packageCount++
-		time.Sleep(1 * time.Second) // RdH todo delete
+		// time.Sleep(1 * time.Second) // RdH todo delete
 	}
 
 	print(id, "rdh here")
 	connectionEndpoint.Close()
 	print(id, "TCP connection with localhost:9000 closed")
 	// connection.Close() // RdH: Probably don't need it (connection closed by the client)
-	// log.Printf("TCP connection with %v closed", connection.LocalAddr())
 }
 
 func main() {
@@ -156,7 +100,6 @@ func main() {
 
 	for {
 		connection, err := tcpListener.Accept()
-
 		id, _ := GenerateRandomString(5)
 
 		log.Printf("%v - TCP Connection accepted: %v", id, connection.LocalAddr())
@@ -166,6 +109,7 @@ func main() {
 		}
 
 		go proxyRequest(id, connection)
+		// proxyRequest(id, connection)
 	}
 
 }
